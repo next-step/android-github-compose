2cfa1ecc97b99d80716f264e1b726caaabb157ca
jay.lee <stkment@gmail.com>
2024-08-29 00:31:55 +0900

    feat: 저장소 목록 불러오기 및 로그 남기기

--- app/src/main/java/nextstep/github/MainActivity.kt
+++ app/src/main/java/nextstep/github/MainActivity.kt
@@ -1,6 +1,7 @@
 package nextstep.github
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import nextstep.github.ui.theme.GithubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val appContainer = (application as MainApplication).appContainer
+        lifecycleScope.launch {
+            val result =
+                appContainer.githubRepository
+                    .getRepositories("next-step")
+
+            Log.d("MainActivity", "result: $result")
+        }
         setContent {
             GithubTheme {
                 // A surface container using the 'background' color from the theme

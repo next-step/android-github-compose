9eda4447817e793645106ccd7f4700439ddf98dc
yongho2157 <youngho2157@naver.com>
2024-09-03 01:31:50 +0900

    test: 스낵바 노출 테스트 코드 구현

--- app/src/androidTest/java/nextstep/github/HomeScreenTest.kt
+++ app/src/androidTest/java/nextstep/github/HomeScreenTest.kt
@@ -3,7 +3,7 @@ package nextstep.github
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import nextstep.github.ui.home.HomeScreen
@@ -22,23 +22,20 @@ class HomeScreenTest {
     @Before
     fun setup() {
         composeTestRule.setContent {
-            HomeScreen(homeUiState = homeUiState)
+            HomeScreen(
+                homeUiState = homeUiState,
+                onRetry = { }
+            )
         }
     }
 
     @Test
-    fun 데이터를_정상적으로_받으면_리스트에_노출된다() {
-        composeTestRule
-            .onNodeWithText("next-step")
-            .isDisplayed()
-    }
+    fun 데이터를_가져오지_못하면_스낵바가_노출된다() {
+        homeUiState = HomeUiState.NoRepos(errorMessage = "next-step Repositories not found")
 
-    @Test
-    fun 데이터를_가져오지_못하면_에러_텍스트가_노출된다() {
-        homeUiState = HomeUiState.NoRepos("next-step Repositories not found")
         composeTestRule
-            .onNodeWithText("next-step Repositories not found")
-            .isDisplayed()
+            .onNodeWithText("예상치 못한 오류가 발생했습니다.")
+            .assertIsDisplayed()
     }
 
 }

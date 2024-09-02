030bc850c0667fb881a76e85d713c19f189c960f
Jihoi-Kang <jihoi.kang@gmail.com>
2024-09-02 10:11:15 +0900

    refactor: add topbar preview

--- app/src/main/java/nextstep/github/ui/repository/component/RepositoryListTopBar.kt
+++ app/src/main/java/nextstep/github/ui/repository/component/RepositoryListTopBar.kt
@@ -5,10 +5,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import nextstep.github.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RepositoryListTopBar() {
     CenterAlignedTopAppBar(title = { Text(text = stringResource(id = R.string.nextstep_repositories)) })
+}
+
+@Preview
+@Composable
+private fun RepositoryListTopBarPreview() {
+    RepositoryListTopBar()
 }

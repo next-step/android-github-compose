0ba6a5e0a6c4e1b29b0923a7b5e83a472e78a0b2
junbo sim <junbo.sim@lemonhc.com>
2024-09-03 14:20:50 +0900

    refactor: datasource 구현체 네이밍 변경

--- app/src/main/java/nextstep/github/data/source/GithubRemoteDataSource.kt
+++ app/src/main/java/nextstep/github/data/source/GithubRemoteDataSource.kt
@@ -4,7 +4,7 @@ import nextstep.github.data.model.toGithubRepo
 import nextstep.github.data.service.GithubService
 import nextstep.github.model.GithubRepo
 
-class GithubDataSourceImpl(
+class GithubRemoteDataSource(
     private val githubService: GithubService,
 ) : GithubDataSource {
     override suspend fun getRepositories(organization: String): List<GithubRepo> {

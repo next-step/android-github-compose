7244068300f0e7a78f47fb6dd431513ddb6948e8
Yongho Choi <73802331+yongho2157@users.noreply.github.com>
2024-09-03 21:42:12 +0900

    3단계 - GitHub(UI 상태) (#21)
    * docs: 기능 구현 목록 추가
    
    * feat: GitHub(UI 상태) 구현
    
    * test: 스낵바 노출 테스트 코드 구현

--- app/src/main/java/nextstep/github/ui/components/GithubTopBar.kt
+++ app/src/main/java/nextstep/github/ui/components/GithubTopBar.kt
@@ -5,12 +5,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GithubTopBar(
-    title: String
+    title: String,
+    modifier: Modifier = Modifier
 ) {
     CenterAlignedTopAppBar(
         title = {
--- app/src/main/java/nextstep/github/ui/home/HomeScreen.kt
+++ app/src/main/java/nextstep/github/ui/home/HomeScreen.kt
@@ -2,23 +2,34 @@ package nextstep.github.ui.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -29,16 +40,15 @@ import nextstep.github.ui.theme.GithubTheme
 
 @Composable
 fun HomeScreen(
+    modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = HomeViewModel.Factory),
-    modifier: Modifier = Modifier
 ) {
 
     val homeUiState by viewModel.homeUiState.collectAsStateWithLifecycle()
 
-    viewModel.fetchRepos("next-step")
-
     HomeScreen(
         homeUiState = homeUiState,
+        onRetry = { viewModel.fetchRepos("next-step") },
         modifier = modifier
     )
 
@@ -47,9 +57,15 @@ fun HomeScreen(
 @Composable
 fun HomeScreen(
     homeUiState: HomeUiState,
+    onRetry: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
         topBar = {
             GithubTopBar(
                 title = stringResource(id = R.string.github_top_bar_title)
@@ -57,53 +73,116 @@ fun HomeScreen(
         },
         modifier = modifier
     ) { innerPadding ->
-        when (homeUiState) {
-            is HomeUiState.HasRepos -> {
-                LazyColumn(
-                    modifier = Modifier.padding(innerPadding)
-                ) {
-                    items(homeUiState.githubRepos) { githubRepo ->
+        LoadingContent(
+            empty = when (homeUiState) {
+                is HomeUiState.HasRepos -> false
+                is HomeUiState.NoRepos -> homeUiState.isLoading
+            },
+            emptyContent = { FullScreenLoading() },
+        ) {
+            when (homeUiState) {
+                is HomeUiState.NoRepos -> {
+                    if (homeUiState.errorMessage.isEmpty()) {
                         Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             Text(
-                                text = githubRepo.fullName,
-                                style = MaterialTheme.typography.titleLarge
-                            )
-                            Text(
-                                text = githubRepo.description,
-                                style = MaterialTheme.typography.bodyMedium
+                                text = stringResource(id = R.string.empty_repos),
+                                style = MaterialTheme.typography.headlineSmall
                             )
                         }
-                        HorizontalDivider()
+                    }
+                }
+
+                is HomeUiState.HasRepos -> {
+                    LazyColumn(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        items(homeUiState.githubRepos) { githubRepo ->
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            ) {
+                                Text(
+                                    text = githubRepo.fullName,
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                                Text(
+                                    text = githubRepo.description,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                            HorizontalDivider()
+                        }
                     }
                 }
             }
-
-            is HomeUiState.NoRepos -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = homeUiState.errorMessage)
+        }
+        if (homeUiState.errorMessage.isNotEmpty()) {
+            val errorMessage = remember(homeUiState) { homeUiState.errorMessage }
+            val errorMessageText: String = stringResource(R.string.snack_bar_title)
+            val retryMessageText = stringResource(id = R.string.retry)
+            LaunchedEffect(errorMessage) {
+                val result = snackbarHostState
+                    .showSnackbar(
+                        message = errorMessageText,
+                        actionLabel = retryMessageText,
+                        duration = SnackbarDuration.Indefinite
+                    )
+                if (result == SnackbarResult.ActionPerformed) {
+                    onRetry()
                 }
             }
         }
 
+
     }
+}
+
+@Composable
+private fun LoadingContent(
+    empty: Boolean,
+    emptyContent: @Composable () -> Unit,
+    content: @Composable () -> Unit
+) {
+    if (empty) {
+        emptyContent()
+    } else {
+        content()
+    }
+}
+
+@Composable
+private fun FullScreenLoading() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+class HomeScreenProvider : PreviewParameterProvider<HomeUiState> {
+    override val values: Sequence<HomeUiState> = sequenceOf(
+        HomeUiState.HasRepos(dummyData),
+        HomeUiState.NoRepos(errorMessage = "", isLoading = false)
+    )
 }
 
 @SuppressLint("UnrememberedMutableState")
 @Preview
 @Composable
-private fun HomeScreenPreview() {
-
-    val homeUiState by mutableStateOf(HomeUiState.HasRepos(dummyData))
-
+private fun HomeScreenPreview(
+    @PreviewParameter(HomeScreenProvider::class) homeUiState: HomeUiState
+) {
     GithubTheme {
-        HomeScreen(homeUiState = homeUiState)
+        HomeScreen(
+            homeUiState = homeUiState,
+            onRetry = { }
+        )
     }
 }

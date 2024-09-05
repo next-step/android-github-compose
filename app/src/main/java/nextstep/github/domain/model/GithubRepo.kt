847c20abf8bbe65f0809e9cc0b5aadd50a913464
yongho2157 <youngho2157@naver.com>
2024-09-05 23:36:43 +0900

    refactor: 데이터 일관성 보장되도록 변경

--- app/src/main/java/nextstep/github/domain/model/GithubRepo.kt
+++ app/src/main/java/nextstep/github/domain/model/GithubRepo.kt
@@ -3,22 +3,22 @@ package nextstep.github.domain.model
 data class GithubRepo(
     val fullName: String,
     val description: String,
-    val stars: Int,
+    val stars: Int
+) {
     val isHotRepo: Boolean
-)
+        get() = stars >= 50
+}
 
 val dummyGithubRepo1 =
     GithubRepo(
         fullName = "next-step/android-github-compose",
         description = "권장 앱 아키텍처",
-        stars = 50,
-        isHotRepo = true
+        stars = 50
     )
 
 val dummyGithubRepo2 =
     GithubRepo(
         fullName = "next-step/android-github-compose",
         description = "권장 앱 아키텍처",
-        stars = 49,
-        isHotRepo = false
+        stars = 49
     )

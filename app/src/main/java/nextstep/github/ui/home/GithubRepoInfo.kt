91dc520c78b44b0f3b409f87de2e4edd8a277296
yongho2157 <youngho2157@naver.com>
2024-09-06 00:07:01 +0900

    refactor: GithubRepoHeaderInfo 분리

--- app/src/main/java/nextstep/github/ui/home/GithubRepoInfo.kt
+++ app/src/main/java/nextstep/github/ui/home/GithubRepoInfo.kt
@@ -1,24 +1,19 @@
 package nextstep.github.ui.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import nextstep.github.R
 import nextstep.github.domain.model.GithubRepo
-import nextstep.github.domain.model.dummyDefaultGithubRepo
+import nextstep.github.domain.model.dummyGithubRepoHot
+import nextstep.github.domain.model.dummyGithubRepoNonHot
 
 @Composable
 fun GithubRepoInfo(
@@ -30,32 +25,7 @@ fun GithubRepoInfo(
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            if (githubRepo.isHotRepo) {
-                Text(
-                    text = stringResource(id = R.string.repo_hot),
-                    style = MaterialTheme.typography.labelLarge,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Star,
-                    contentDescription = stringResource(id = R.string.repo_star_icon_description)
-                )
-                Text(
-                    text = githubRepo.stars.toString(),
-                    style = MaterialTheme.typography.labelLarge,
-                )
-            }
-        }
+        RepoHeaderInfo(githubRepo = githubRepo)
         Text(
             text = githubRepo.fullName,
             style = MaterialTheme.typography.titleLarge
@@ -67,8 +37,17 @@ fun GithubRepoInfo(
     }
 }
 
+class GithubRepoInfoProvider : PreviewParameterProvider<GithubRepo> {
+    override val values: Sequence<GithubRepo> = sequenceOf(
+        dummyGithubRepoHot,
+        dummyGithubRepoNonHot
+    )
+}
+
 @Preview
 @Composable
-private fun GithubRepoInfoPreview() {
-    GithubRepoInfo(githubRepo = dummyDefaultGithubRepo)
+private fun GithubRepoInfoPreview(
+    @PreviewParameter(GithubRepoInfoProvider::class) githubRepo: GithubRepo
+) {
+    GithubRepoInfo(githubRepo = githubRepo)
 }

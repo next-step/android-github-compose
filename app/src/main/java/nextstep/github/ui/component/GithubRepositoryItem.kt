b70a6d2440557a43d07e7134153d4634a75b7854
ezhoon <dlwlgns1240@gmail.com>
2024-09-06 20:37:24 +0900

    refactor: ellipsis 추가

--- app/src/main/java/nextstep/github/ui/component/GithubRepositoryItem.kt
+++ app/src/main/java/nextstep/github/ui/component/GithubRepositoryItem.kt
@@ -5,7 +5,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import nextstep.github.ui.theme.GithubTheme
 
 @Composable
@@ -21,24 +24,35 @@ fun GithubRepositoryItem(
             text = fullName,
             style = MaterialTheme.typography.titleLarge,
             color = MaterialTheme.colorScheme.onSurface,
-            maxLines = 1
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
         Text(
             text = description,
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.colorScheme.onSurface,
-            maxLines = 1
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
 
+private class GithubRepositoryItemProvider : PreviewParameterProvider<Pair<String, String>> {
+    override val values: Sequence<Pair<String, String>> = sequenceOf(
+        "next-step/nextstep-docs" to "nextstep 매뉴얼 및 문서를 관리하는 저장소",
+        "next-step/nextstep-docs" to "nextstep 매뉴얼 및 문서를 관리하는 저장소 nextstep 매뉴얼 및 문서를 관리하는 저장소 ",
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-private fun GithubRepositoryItemPreview() {
+private fun GithubRepositoryItemPreview(
+    @PreviewParameter(GithubRepositoryItemProvider::class) repository: Pair<String, String>,
+) {
     GithubTheme {
         GithubRepositoryItem(
-            fullName = "next-step/nextstep-docs",
-            description = "nextstep 매뉴얼 및 문서를 관리하는 저장소"
+            fullName = repository.first,
+            description = repository.second
         )
     }
 }

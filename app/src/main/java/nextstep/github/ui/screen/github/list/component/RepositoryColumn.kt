350adaae8b76b04f35e60221d9d8cbc14731441e
OYJ <oyj7677@gmail.com>
2024-09-07 17:44:31 +0900

    Feat : 깃허브 저장소 star 표시 화면 구현
    1. 저장소 칼럼 헤더에 Star 개수 노출
    2.  Star 개수 50개 이상일 시 HOT 문구 노출

--- app/src/main/java/nextstep/github/ui/screen/github/list/component/RepositoryColumn.kt
+++ app/src/main/java/nextstep/github/ui/screen/github/list/component/RepositoryColumn.kt
@@ -2,15 +2,23 @@ package nextstep.github.ui.screen.github.list.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.github.R
 import nextstep.github.core.data.GithubRepositoryInfo
 
 @Composable
@@ -20,6 +28,8 @@ fun RepositoryColumn(repositoryInfo: GithubRepositoryInfo, modifier: Modifier = 
             .padding(vertical = 16.dp)
             .fillMaxWidth()
     ) {
+        RepositoryColumnHeader(repositoryInfo = repositoryInfo)
+
         Text(
             text = repositoryInfo.fullName,
             style = MaterialTheme.typography.titleLarge,

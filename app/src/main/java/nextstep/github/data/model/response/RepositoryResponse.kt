dd2e5346aa270b5ea9d0c52cbad6492db6e62df3
eshc123 <>
2024-09-07 12:14:12 +0900

    refactor: RepositoryEntity -> RepositoryResponse 네이밍 수정

--- app/src/main/java/nextstep/github/data/model/response/RepositoryResponse.kt
+++ app/src/main/java/nextstep/github/data/model/response/RepositoryResponse.kt
@@ -1,10 +1,10 @@
-package nextstep.github.data.entity
+package nextstep.github.data.model.response
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RepositoryEntity(
+data class RepositoryResponse(
     @SerialName("full_name") val fullName: String?,
     @SerialName("description") val description: String?,
 )
--- app/src/main/java/nextstep/github/data/service/GithubService.kt
+++ app/src/main/java/nextstep/github/data/service/GithubService.kt
@@ -1,6 +1,6 @@
 package nextstep.github.data.service
 
-import nextstep.github.data.entity.RepositoryEntity
+import nextstep.github.data.model.response.RepositoryResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,5 +9,5 @@ interface GithubService {
     @GET("orgs/{organization}/repos")
     suspend fun getRepositories(
         @Path("organization") organization: String
-    ): List<RepositoryEntity>
+    ): List<RepositoryResponse>
 }

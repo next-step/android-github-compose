cb17decfeaaf928bb3fb330a2511cf3abf0f0f63
eshc123 <>
2024-09-07 13:43:39 +0900

    feat: 저장소 목록 화면 - RepositoryListScreen 구현

--- app/src/main/java/nextstep/github/MainActivity.kt
+++ app/src/main/java/nextstep/github/MainActivity.kt
@@ -1,42 +1,19 @@
 package nextstep.github
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import kotlinx.coroutines.launch
-import nextstep.github.data.repository.GithubRepository
+import nextstep.github.ui.screen.RepositoryListRouteScreen
 import nextstep.github.ui.theme.GithubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val appContainer = (application as MainApplication).appContainer
-        initObservers(appContainer.githubRepository)
-
         setContent {
-            GithubTheme { }
-        }
-    }
-
-    private fun initObservers(repository : GithubRepository){
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                getRepositories(repository)
+            GithubTheme {
+                RepositoryListRouteScreen()
             }
-        }
-    }
-
-    private suspend fun getRepositories(repository : GithubRepository){
-        try {
-            val repositories = repository.getRepositories("next-step")
-            Log.d("MainActivity",repositories.joinToString("\n"))
-        }catch (e: Exception){
-            Log.d("MainActivity",e.toString())
         }
     }
 }

238ca168f44bc2132aeeb88d9e379821e6d69f2a
eshc123 <>
2024-09-07 13:44:00 +0900

    chore: 불필요한 import 제거

--- app/src/main/java/nextstep/github/data/repository/GithubRepository.kt
+++ app/src/main/java/nextstep/github/data/repository/GithubRepository.kt
@@ -1,7 +1,6 @@
 package nextstep.github.data.repository
 
 import nextstep.github.data.model.response.RepositoryResponse
-import nextstep.github.ui.model.RepositoryModel
 
 interface GithubRepository {
     suspend fun getRepositories(organization: String): List<RepositoryResponse>
--- app/src/main/java/nextstep/github/data/repository/GithubRepositoryImpl.kt
+++ app/src/main/java/nextstep/github/data/repository/GithubRepositoryImpl.kt
@@ -2,8 +2,6 @@ package nextstep.github.data.repository
 
 import nextstep.github.data.model.response.RepositoryResponse
 import nextstep.github.data.service.GithubService
-import nextstep.github.ui.model.RepositoryModel
-import nextstep.github.ui.model.toModel
 
 class GithubRepositoryImpl(
     private val githubService: GithubService
@@ -11,5 +9,4 @@ class GithubRepositoryImpl(
     override suspend fun getRepositories(organization: String): List<RepositoryResponse> {
         return githubService.getRepositories(organization)
     }
-
 }

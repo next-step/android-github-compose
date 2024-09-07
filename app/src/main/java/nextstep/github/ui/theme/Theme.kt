7f528c68d86c2955eba230356336798345764b29
eshc123 <>
2024-09-07 13:38:55 +0900

    feat: Color 추가

--- app/src/main/java/nextstep/github/ui/theme/Theme.kt
+++ app/src/main/java/nextstep/github/ui/theme/Theme.kt
@@ -13,29 +13,22 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    onBackground = Grey80,
+    onSurface = Grey100
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Pink40,
+    onBackground = Grey20,
+    onSurface = Grey00
 )
 
 @Composable
 fun GithubTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {

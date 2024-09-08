a3b37f8a57aa5987371260eb3279251d04e932da
simjunbo <simuelunbo@gmail.com>
2024-09-08 19:43:40 +0900

    refactor: 문자열 string resource로 관리 처리

--- app/src/main/java/nextstep/github/ui/github/GithubRepoListScreen.kt
+++ app/src/main/java/nextstep/github/ui/github/GithubRepoListScreen.kt
@@ -15,10 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import nextstep.github.R
 import nextstep.github.model.GithubRepo
 import nextstep.github.ui.github.component.GithubRepoItem
 import nextstep.github.ui.theme.GithubTheme
@@ -41,7 +43,7 @@ private fun GithubRepositoryListScreen(
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text("NEXTSTEP Repositories") }
+                title = { Text(stringResource(R.string.top_bar_title)) }
             )
         }
     ) {

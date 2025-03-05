e2354eefc6e0ac9fec2950fd24c063f4c039d4ec
parkduksung <duksung1234@naver.com>
2025-03-06 08:53:22 +0900

    feat - Log 결과 확인

--- app/src/main/java/nextstep/github/MainActivity.kt
+++ app/src/main/java/nextstep/github/MainActivity.kt
@@ -1,6 +1,7 @@
 package nextstep.github
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.github.ui.theme.GithubTheme
@@ -15,9 +17,16 @@ import nextstep.github.ui.theme.GithubTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val appContainer = (application as GithubApplication).appContainer
+        val githubRepository = appContainer.githubRepository
+
         setContent {
             GithubTheme {
                 // A surface container using the 'background' color from the theme
+                LaunchedEffect(Unit) {
+                    Log.d("결과",githubRepository.getRepositories("next-step").toString())
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background

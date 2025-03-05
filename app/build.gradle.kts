0e1e9848fc5b60f61d200f55bb105b8920048957
wisemuji <wisemuji@gmail.com>
2025-03-05 17:18:45 +0900

    build: upgrade to android gradle plugin 8.5.2 and apply Compose Gradle Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "nextstep.github"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "nextstep.github"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -38,9 +39,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {

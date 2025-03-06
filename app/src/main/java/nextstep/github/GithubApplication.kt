6d2dba3f583657c692da9ce50b6f3b0600c3e669
sanga.oh2 <66289794+sana-20@users.noreply.github.com>
2025-03-06 15:40:10 +0900

    🚀 1단계 - GitHub(데이터 레이어) (#41)
    * docs: step1 기능 목록 작성
    
    * feat: 의존성 추가
    
    * feat: github 저장소 목록을 가져오는 Client를 구현
    
    * feat: 실제 서버 데이터 확인하는 로그 추가

---
+++ app/src/main/java/nextstep/github/GithubApplication.kt
@@ -0,0 +1,10 @@
+package nextstep.github
+
+import android.app.Application
+import nextstep.github.di.AppContainer
+
+class GithubApplication : Application() {
+
+    val appContainer = AppContainer()
+    
+}
--- app/src/main/java/nextstep/github/MainActivity.kt
+++ app/src/main/java/nextstep/github/MainActivity.kt
@@ -1,46 +1,43 @@
 package nextstep.github
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import nextstep.github.ui.theme.GithubTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        fetchTest()
         setContent {
             GithubTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun fetchTest() {
+        val appContainer = (application as GithubApplication).appContainer
+        val repository = appContainer.githubRepository
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GithubTheme {
-        Greeting("Android")
+        CoroutineScope(Dispatchers.IO).launch {
+            val repos = repository.getRepositories("next-step")
+            Log.d("sana", "repos = $repos")
+        }
     }
+
 }

cb59441a0f7c644997c8e9e1d08cc535eb43f0e3
parkduksung <duksung1234@naver.com>
2025-03-07 08:21:31 +0900

    test - GithubViewModelTest 추가.

---
+++ app/src/test/java/nextstep/github/GithubViewModelTest.kt
@@ -0,0 +1,46 @@
+package nextstep.github
+
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import nextstep.github.data.api.model.RepositoryEntity
+import nextstep.github.data.repository.GithubRepository
+import nextstep.github.ui.github.GithubViewModel
+import org.junit.Test
+
+class GithubViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `레포지토리_결과값이_올바르게_전달되어야_한다`() = runTest {
+        //given
+        val repositories = listOf(
+            RepositoryEntity(
+                fullName = "next-step/nextstep-docs1",
+                description = "nextstep 매뉴얼 및 문서를 관리하는 저장소1"
+            ),
+            RepositoryEntity(
+                fullName = "next-step/nextstep-docs2",
+                description = "nextstep 매뉴얼 및 문서를 관리하는 저장소2"
+            )
+        )
+        val githubViewModel = GithubViewModel(fakeGithubRepository(repositories))
+
+        //when
+        githubViewModel.getRepositories()
+
+        advanceUntilIdle()
+
+        //then
+        assertEquals(repositories, githubViewModel.repositories.value)
+    }
+
+    companion object {
+        fun fakeGithubRepository(repositories: List<RepositoryEntity>): GithubRepository =
+            object : GithubRepository {
+                override suspend fun getRepositories(organization: String): List<RepositoryEntity> =
+                    repositories
+            }
+    }
+}

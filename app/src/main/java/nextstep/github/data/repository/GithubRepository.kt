c8e30a3caed8527ae7b8730fd8ed8b1a705a9b5a
seungohjin <corrobolea@gmail.com>
2025-03-06 21:11:39 +0900

    feat: GithubRepository 추가

---
+++ app/src/main/java/nextstep/github/data/repository/GithubRepository.kt
@@ -0,0 +1,26 @@
+package nextstep.github.data.repository
+
+import nextstep.github.data.model.GithubModel
+import nextstep.github.data.remote.api.GithubService
+
+interface GithubRepository {
+    suspend fun getRepositories(organization: String): Result<List<GithubModel>>
+}
+
+class GithubRepositoryImpl(
+    private val githubService: GithubService,
+) : GithubRepository {
+
+    override suspend fun getRepositories(organization: String): Result<List<GithubModel>> {
+        return runCatching {
+            githubService
+                .getRepositories(organization)
+                .map {
+                    GithubModel(
+                        fullName = it.fullName ?: "",
+                        description = it.description ?: ""
+                    )
+                }
+        }
+    }
+}

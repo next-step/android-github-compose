9020ae874d0842a2362f5bbec9ac15e8d327bc4c
parkduksung <duksung1234@naver.com>
2025-03-07 08:16:21 +0900

    feat - GithubTopBar 추가.

---
+++ app/src/main/java/nextstep/github/ui/github/component/GithubTopBar.kt
@@ -0,0 +1,39 @@
+package nextstep.github.ui.github.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.github.R
+import nextstep.github.ui.theme.GithubTheme
+import nextstep.github.ui.theme.TextColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GithubTopBar(
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.title_github),
+                style = MaterialTheme.typography.titleLarge,
+                color = TextColor
+            )
+        },
+        modifier = modifier.fillMaxWidth(),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GithubTopBarPreview() {
+    GithubTheme {
+        GithubTopBar()
+    }
+}

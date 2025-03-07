ed10506b3afdb8befebc1dd2e2919b6289d1e6a7
parkduksung <duksung1234@naver.com>
2025-03-07 20:17:29 +0900

    refactor - 네이밍 수정 및 불필요한 부분 제거.

--- app/src/main/java/nextstep/github/ui/theme/Color.kt
+++ app/src/main/java/nextstep/github/ui/theme/Color.kt
@@ -10,5 +10,4 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val TextColor = Color(0xFF1D1B20)
-val OutlineVariant = Color(0xFFCAC4D0)
+val TopAppBarTitleColor = Color(0xFF1D1B20)

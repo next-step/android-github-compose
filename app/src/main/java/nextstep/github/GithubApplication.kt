584257a3fafdcccf999e88f61fb985fcddf6150e
Bandal <no1msh1217@gmail.com>
2025-03-07 15:54:51 +0900

    feat: AppContainer를 Application 객체에 연결

---
+++ app/src/main/java/nextstep/github/GithubApplication.kt
@@ -0,0 +1,9 @@
+package nextstep.github
+
+import android.app.Application
+import nextstep.github.data.AppContainer
+
+class GithubApplication: Application() {
+
+    val appContainer = AppContainer()
+}

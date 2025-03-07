d07e601011434c843ce2c6b2980c9d1a0c1b4fec
Bandal <no1msh1217@gmail.com>
2025-03-07 15:52:55 +0900

    feat: AppContainer 구현

---
+++ app/src/main/java/nextstep/github/data/AppContainer.kt
@@ -0,0 +1,27 @@
+package nextstep.github.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import nextstep.github.data.repositories.impls.RemoteGithubRepository
+import nextstep.github.data.services.GithubService
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+class AppContainer {
+    private val serialization = Json { ignoreUnknownKeys = true }
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(OkHttpClient.Builder().build())
+        .addConverterFactory(serialization.asConverterFactory(CONTENT_TYPE.toMediaType()))
+        .build()
+    private val githubService = retrofit
+        .create(GithubService::class.java)
+
+    val githubRepository = RemoteGithubRepository(githubService)
+
+        companion object {
+        private const val CONTENT_TYPE = "application/json"
+        private const val BASE_URL = "https://api.github.com"
+    }
+}

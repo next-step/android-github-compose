d1e50fb1153ff6d7c7f50af78f8d662f9616134a
seungohjin <corrobolea@gmail.com>
2025-03-07 21:38:53 +0900

    refactor: GithubResponse > RepositoryResponse로 클래스명 수정

--- app/src/main/java/nextstep/github/data/remote/api/mapper/GithubResponseToDataMapper.kt
+++ app/src/main/java/nextstep/github/data/remote/api/mapper/GithubResponseToDataMapper.kt
@@ -1,11 +1,11 @@
 package nextstep.github.data.remote.api.mapper
 
 import nextstep.github.data.model.GithubModel
-import nextstep.github.data.remote.response.GithubResponse
+import nextstep.github.data.remote.response.RepositoryResponse
 
-fun List<GithubResponse>.toDataList(): List<GithubModel> = map { it.toData() }
+fun List<RepositoryResponse>.toDataList(): List<GithubModel> = map { it.toData() }
 
-fun GithubResponse.toData() = GithubModel(
+fun RepositoryResponse.toData() = GithubModel(
     fullName = fullName ?: "",
     description = description ?: ""
 )
--- app/src/main/java/nextstep/github/data/remote/response/RepositoryResponse.kt
+++ app/src/main/java/nextstep/github/data/remote/response/RepositoryResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GithubResponse(
+data class RepositoryResponse(
     @SerialName("full_name") val fullName: String?,
     @SerialName("description") val description: String?,
 )

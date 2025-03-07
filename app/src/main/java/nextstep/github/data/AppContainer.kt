4624a70f45371472aeaa0fc38d587c8ef5b7c22f
Bandal <no1msh1217@gmail.com>
2025-03-07 16:24:58 +0900

    feat: HttpLoggingInterceptor 구축

--- app/src/main/java/nextstep/github/data/AppContainer.kt
+++ app/src/main/java/nextstep/github/data/AppContainer.kt
@@ -1,26 +1,46 @@
 package nextstep.github.data
 
+import android.util.Log
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import nextstep.github.data.repositories.impls.RemoteGithubRepository
 import nextstep.github.data.services.GithubService
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 
 class AppContainer {
     private val serialization = Json { ignoreUnknownKeys = true }
+
+    private val httpLoggingInterceptor = HttpLoggingInterceptor { message ->
+        Log.d(TAG_HTTP_LOG, message)
+    }.apply { setLevel(HttpLoggingInterceptor.Level.BODY) }
+
+    private val okHttpClient = OkHttpClient
+        .Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .client(OkHttpClient.Builder().build())
+        .client(okHttpClient)
         .addConverterFactory(serialization.asConverterFactory(CONTENT_TYPE.toMediaType()))
         .build()
-    private val githubService = retrofit
-        .create(GithubService::class.java)
+
+    private val githubService = retrofit.create(GithubService::class.java)
 
     val githubRepository = RemoteGithubRepository(githubService)
 
-        companion object {
+    companion object {
+        private const val TAG_HTTP_LOG = "Http_Log"
+        private const val CONNECT_TIMEOUT = 20L
+        private const val WRITE_TIMEOUT = 20L
+        private const val READ_TIMEOUT = 20L
         private const val CONTENT_TYPE = "application/json"
         private const val BASE_URL = "https://api.github.com"
     }

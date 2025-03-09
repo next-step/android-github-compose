1caa72a1e78ec49c512eec98ad6a72a2eec03ce8
Bandal <no1msh1217@gmail.com>
2025-03-09 21:01:06 +0900

    feat: Repository에 fullName을 non-null로 변경

--- app/src/main/java/nextstep/github/model/Repository.kt
+++ app/src/main/java/nextstep/github/model/Repository.kt
@@ -9,7 +9,7 @@ data class Repository(
     val id: Long,
 
     @SerialName("full_name")
-    val fullName: String?,
+    val fullName: String,
 
     @SerialName("description")
     val description: String?,

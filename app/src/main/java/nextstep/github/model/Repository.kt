fa21e05c961ef45cac749f30263a7437739a5bb1
Bandal <no1msh1217@gmail.com>
2025-03-09 20:59:59 +0900

    feat: Repository에 ID 추가

--- app/src/main/java/nextstep/github/model/Repository.kt
+++ app/src/main/java/nextstep/github/model/Repository.kt
@@ -5,6 +5,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Repository(
+    @SerialName("id")
+    val id: Long,
 
     @SerialName("full_name")
     val fullName: String?,

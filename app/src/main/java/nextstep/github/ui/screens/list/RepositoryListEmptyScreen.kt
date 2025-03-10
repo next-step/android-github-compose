26053f8d4cc5afe227a9c44ae4d774a7a59a10be
Bandal <no1msh1217@gmail.com>
2025-03-11 02:02:42 +0900

    refactor: 부속 Screen 함수에서 Scaffold 제거

--- app/src/main/java/nextstep/github/ui/screens/list/RepositoryListEmptyScreen.kt
+++ app/src/main/java/nextstep/github/ui/screens/list/RepositoryListEmptyScreen.kt
@@ -2,9 +2,7 @@ package nextstep.github.ui.screens.list
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,24 +10,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.github.R
-import nextstep.github.ui.components.GithubTopBar
 import nextstep.github.ui.theme.GithubTheme
 
 @Composable
 fun RepositoryListEmptyScreen(modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = { GithubTopBar(title = stringResource(R.string.repository_list_top_bat_title)) },
-        modifier = modifier,
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
     ) {
-        Box(
-            modifier = Modifier.padding(it).fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(
-                text = stringResource(R.string.repository_list_empty),
-                style = MaterialTheme.typography.headlineSmall,
-            )
-        }
+        Text(
+            text = stringResource(R.string.repository_list_empty),
+            style = MaterialTheme.typography.headlineSmall,
+        )
     }
 }
 
--- app/src/main/java/nextstep/github/ui/screens/list/RepositoryListSuccessScreen.kt
+++ app/src/main/java/nextstep/github/ui/screens/list/RepositoryListSuccessScreen.kt
@@ -4,15 +4,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import nextstep.github.R
 import nextstep.github.model.Repository
-import nextstep.github.ui.components.GithubTopBar
 import nextstep.github.ui.screens.list.components.RepositoryItem
 import nextstep.github.ui.theme.GithubTheme
 
@@ -21,23 +17,18 @@ fun RepositoryListSuccessScreen(
     state: RepositoryListUiState.Success,
     modifier: Modifier = Modifier,
 ) {
-    Scaffold(
-        topBar = { GithubTopBar(title = stringResource(R.string.repository_list_top_bat_title)) },
-        modifier = modifier,
-    ) {
-        LazyColumn(modifier = Modifier.padding(it)) {
-            items(key = { item -> item.id }, items = state.repositories) { repository ->
-                RepositoryItem(
-                    repository = repository,
-                    modifier = Modifier.padding(16.dp)
-                )
-                HorizontalDivider()
-            }
+    LazyColumn(modifier = modifier) {
+        items(key = { item -> item.id }, items = state.repositories) { repository ->
+            RepositoryItem(
+                repository = repository,
+                modifier = Modifier.padding(16.dp)
+            )
+            HorizontalDivider()
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun RepositoryListSuccessScreenPreview() {
     GithubTheme {

fae6dfc5eaa3ceb38e88ff9b1185bf2636ac9ad9
Bandal <no1msh1217@gmail.com>
2025-03-11 02:01:11 +0900

    feat: dynamicColor 디폴트 값 false로 변경

--- app/src/main/java/nextstep/github/ui/theme/Theme.kt
+++ app/src/main/java/nextstep/github/ui/theme/Theme.kt
@@ -20,23 +20,12 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun GithubTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {

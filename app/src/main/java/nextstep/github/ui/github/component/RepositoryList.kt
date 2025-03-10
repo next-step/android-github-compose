2c21ac0faf2068b8d0eaa8d95c6305175bca281f
parkduksung <duksung1234@naver.com>
2025-03-10 09:40:58 +0900

    refactor - 수정된 Model 반영.

--- app/src/main/java/nextstep/github/ui/github/component/RepositoryList.kt
+++ app/src/main/java/nextstep/github/ui/github/component/RepositoryList.kt
@@ -10,13 +10,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
-import nextstep.github.data.api.model.RepositoryEntity
+import nextstep.github.model.Repository
 import nextstep.github.ui.theme.GithubTheme
 
 
 @Composable
 fun RepositoryList(
-    items: List<RepositoryEntity>,
+    items: List<Repository>,
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(
@@ -38,7 +38,7 @@ fun RepositoryList(
 @Composable
 private fun RepositoryListPreview() {
     val list = (1..5).map {
-        RepositoryEntity(
+        Repository(
             id = it,
             fullName = "next-step/nextstep-docs${it}",
             description = "nextstep 매뉴얼 및 문서를 관리하는 저장소${it}"

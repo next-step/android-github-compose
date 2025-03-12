c7508a3ca0e2cb1eac8d345a8acd6063eecc5439
raindragonn <raindragonn@gmail.com>
2025-03-12 13:44:46 +0900

    feat: Repository id 추가

--- app/src/main/java/nextstep/github/domain/model/Repository.kt
+++ app/src/main/java/nextstep/github/domain/model/Repository.kt
@@ -3,16 +3,17 @@ package nextstep.github.domain.model
 import nextstep.github.data.model.dto.RepositoryResponse
 
 data class Repository(
+    val id: Int,
     val fullName: String,
     val description: String,
 ) {
     companion object {
-        private const val DEFAULT_NAME = ""
         private const val DEFAULT_DESCRIPTION = ""
 
         fun fromResponse(response: RepositoryResponse) =
             Repository(
-                fullName = response.fullName ?: DEFAULT_NAME,
+                id = response.id,
+                fullName = response.fullName,
                 description = response.description ?: DEFAULT_DESCRIPTION
             )
     }

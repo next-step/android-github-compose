51367b6a08d830721d5897513273230749e3dcce
raindragonn <raindragonn@gmail.com>
2025-03-13 11:06:04 +0900

    test: 보일러 플레이트 제거 - @before 적용

--- app/src/androidTest/java/nextstep/github/ui/list/GitHubRepositoryListScreenTest.kt
+++ app/src/androidTest/java/nextstep/github/ui/list/GitHubRepositoryListScreenTest.kt
@@ -1,11 +1,15 @@
 package nextstep.github.ui.list
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performScrollToIndex
 import nextstep.github.domain.model.Repository
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,13 +18,20 @@ class GitHubRepositoryListScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun `리스트_스크린의_타이틀이_보인다`() {
+    private var repositories by mutableStateOf(emptyList<Repository>())
+
+    @Before
+    fun setup() {
         composeTestRule.setContent {
             GitHubRepositoryListScreen(
-                repositories = emptyList(),
+                repositories = repositories,
             )
         }
+    }
+
+    @Test
+    fun `리스트_스크린의_타이틀이_보인다`() {
+        repositories = emptyList()
 
         composeTestRule
             .onNodeWithText("NEXTSTEP Repositories")
@@ -29,17 +40,11 @@ class GitHubRepositoryListScreenTest {
 
     @Test
     fun `리스트_목록을_보여준다`() {
-        val repositories = List(100) {
+        repositories = List(100) {
             Repository(
                 id = it,
                 fullName = "풀무원말고풀네임$it",
                 description = "설탕말고설명$it"
-            )
-        }
-
-        composeTestRule.setContent {
-            GitHubRepositoryListScreen(
-                repositories = repositories,
             )
         }
 

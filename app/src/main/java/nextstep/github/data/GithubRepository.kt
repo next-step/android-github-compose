c9d7c7460685d1f4399038c50144dd567269e694
yangsooplus <yangsooplus@naver.com>
2025-03-13 23:24:22 +0900

    feat: GithubRepository 인터페이스, 구현체 추가

---
+++ app/src/main/java/nextstep/github/data/GithubRepository.kt
@@ -0,0 +1,12 @@
+package nextstep.github.data
+
+interface GithubRepository {
+    suspend fun getRepositories(organization: String): List<RepositoryEntity>
+}
+
+class GithubRepositoryImpl(private val githubService: GithubService) : GithubRepository {
+
+    override suspend fun getRepositories(organization: String): List<RepositoryEntity> {
+        return githubService.getRepositories(organization)
+    }
+}

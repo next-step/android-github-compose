f609da78848b42862b4a8cd11c1f2e2bbd798874
seungohjin <corrobolea@gmail.com>
2025-03-14 19:18:30 +0900

    test: StatelessGithubScreenTest 스낵바 테스트 코드 추가

--- app/src/androidTest/java/StatelessGithubScreenTest.kt
+++ app/src/androidTest/java/StatelessGithubScreenTest.kt
@@ -1,10 +1,16 @@
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import nextstep.github.ui.screen.github.GithubScreen
 import nextstep.github.ui.screen.github.RepositoryUiState
 import nextstep.github.ui.uistate.UiState
@@ -76,5 +82,71 @@ class StatelessGithubScreenTest {
 
         composeTestRule.onNodeWithContentDescription("LoadingProgressBar")
             .assertIsDisplayed()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun 오류_발생시_화면에_스낵바가_호출된다() = runTest {
+
+        val snackbarHostState = SnackbarHostState()
+
+        composeTestRule.setContent {
+            GithubScreen(
+                repositoryUiState = UiState.Empty,
+                snackbarHostState = snackbarHostState
+            )
+        }
+
+        launch {
+            snackbarHostState.showSnackbar(
+                message = "예상치 못한 오류가 발생했습니다.",
+                actionLabel = "재시도",
+            )
+        }
+
+        advanceUntilIdle()
+
+        composeTestRule.onNodeWithContentDescription("Snackbar")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("예상치 못한 오류가 발생했습니다.")
+            .assertIsDisplayed()
+
+        snackbarHostState.currentSnackbarData?.dismiss()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun 스낵바_재시도_버튼을_클릭하면_스낵바가_사라진다() = runTest {
+        val snackbarHostState = SnackbarHostState()
+
+        composeTestRule.setContent {
+            GithubScreen(
+                repositoryUiState = UiState.Empty,
+                snackbarHostState = snackbarHostState
+            )
+        }
+
+        launch {
+            when (snackbarHostState.showSnackbar(
+                message = "예기치 못한 오류",
+                actionLabel = "재시도"
+            )) {
+                SnackbarResult.ActionPerformed -> {
+                    snackbarHostState.currentSnackbarData?.dismiss()
+                }
+
+                else -> {}
+            }
+        }
+
+        advanceUntilIdle()
+
+        snackbarHostState.currentSnackbarData?.performAction()
+
+        advanceUntilIdle()
+
+        composeTestRule.onNodeWithContentDescription("Snackbar")
+            .assertIsNotDisplayed()
     }
 }

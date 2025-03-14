6dfcbb80ffde1f7023f5112ceb52a7c38bc80d9d
ver <ver@sendy.ai>
2025-03-15 07:49:27 +0900

    feat: 불러온 저장소 목록을 모델로 맵핑해서 전달한다

--- app/src/main/java/nextstep/github/data/dto/RepositoryResponse.kt
+++ app/src/main/java/nextstep/github/data/dto/RepositoryResponse.kt
@@ -2,9 +2,15 @@ package nextstep.github.data.dto
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import nextstep.github.model.GithubRepo
 
 @Serializable
 data class RepositoryResponse(
     @SerialName("full_name") val fullName: String?,
     @SerialName("description") val description: String?,
+)
+
+fun RepositoryResponse.toGithubRepo() = GithubRepo(
+    fullName = this.fullName ?: "",
+    description = this.description ?: "",
 )

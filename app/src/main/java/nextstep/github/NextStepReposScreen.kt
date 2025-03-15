a3244692985b58e93c8858ffd7d77a53891df3b1
ver <ver@sendy.ai>
2025-03-15 23:44:40 +0900

    feat: TopBar 밑에 구분선을 추가한다

--- app/src/main/java/nextstep/github/NextStepReposScreen.kt
+++ app/src/main/java/nextstep/github/NextStepReposScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -48,7 +47,6 @@ private fun NextStepReposScreen(
             NextStepRepoTopBar()
         }) { innerPadding ->
         LazyColumn(modifier = modifier.padding(innerPadding)) {
-            item { HorizontalDivider() }
             items(
                 items = nextStepRepos,
                 key = { item -> item.fullName }) { githubRepo ->
@@ -62,15 +60,18 @@ private fun NextStepReposScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NextStepRepoTopBar(modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
-        title = {
-            Text(
-                text = stringResource(R.string.nextstep_repos_top_bar_title),
-                style = MaterialTheme.typography.titleLarge
-            )
-        },
-        modifier = modifier,
-    )
+    Column {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.nextstep_repos_top_bar_title),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            },
+            modifier = modifier,
+        )
+        HorizontalDivider()
+    }
 }
 
 @Composable

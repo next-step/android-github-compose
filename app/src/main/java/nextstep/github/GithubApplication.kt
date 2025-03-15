5d9f947657d00561c46d796b3af475791d48a191
ver <ver@sendy.ai>
2025-03-15 09:06:30 +0900

    feat: ViewModel, Repository 의존성 주입

---
+++ app/src/main/java/nextstep/github/GithubApplication.kt
@@ -0,0 +1,8 @@
+package nextstep.github
+
+import android.app.Application
+import nextstep.github.di.AppContainer
+
+class GithubApplication : Application() {
+    val appContainer = AppContainer()
+}

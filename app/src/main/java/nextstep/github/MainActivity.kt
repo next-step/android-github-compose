5030c6152110e840e054edc48a98bb9fcd56f289
ver <ver@sendy.ai>
2025-03-15 23:22:00 +0900

    feat: NEXTSTEP 조직의 저장소 목록을 선형 리스트로 노출한다.

--- app/src/main/java/nextstep/github/MainActivity.kt
+++ app/src/main/java/nextstep/github/MainActivity.kt
@@ -1,29 +1,21 @@
 package nextstep.github
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var githubViewModel: GithubViewModel
+    private val githubViewModel: GithubViewModel by viewModels { GithubViewModel.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        githubViewModel = (application as GithubApplication).appContainer.githubViewModel
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                githubViewModel.githubRepositories
-                    .collectLatest {
-                        Log.d("asdf", it.toString())
-                    }
-            }
+        setContent {
+            NextStepReposScreen(
+                viewModel = githubViewModel
+            )
         }
     }
 }
+

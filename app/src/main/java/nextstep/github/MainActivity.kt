bbe76fea09d279a2f6c5777df7818f60e57ab85c
sana-20 <sana20.dev@gmail.com>
2025-03-16 16:08:23 +0900

    refactor: 테스트 코드 제거

--- app/src/main/java/nextstep/github/MainActivity.kt
+++ app/src/main/java/nextstep/github/MainActivity.kt
@@ -1,16 +1,12 @@
 package nextstep.github
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import nextstep.github.ui.theme.GithubTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +14,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        fetchTest()
         setContent {
             GithubTheme {
                 Surface(
@@ -27,16 +22,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                 }
             }
-        }
-    }
-
-    private fun fetchTest() {
-        val appContainer = (application as GithubApplication).appContainer
-        val repository = appContainer.githubRepository
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val repos = repository.getRepositories("next-step")
-            Log.d("sana", "repos = $repos")
         }
     }
 

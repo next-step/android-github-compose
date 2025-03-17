a17cb23772765fb230a8050a58786a6f4dccb930
seungohjin <corrobolea@gmail.com>
2025-03-17 10:58:02 +0900

    test: StatelessGithubScreenTest 테스트 케이스 추가

--- app/src/androidTest/java/StatelessGithubScreenTest.kt
+++ app/src/androidTest/java/StatelessGithubScreenTest.kt
@@ -1,18 +1,22 @@
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
+import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onParent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import nextstep.github.ui.screen.github.GithubScreen
-import nextstep.github.ui.screen.github.RepositoryUiState
+import nextstep.github.ui.screen.github.RepositoryUiModel
 import nextstep.github.ui.uistate.UiState
 import org.junit.Rule
 import org.junit.Test
@@ -24,20 +28,26 @@ class StatelessGithubScreenTest {
 
     val uiState = UiState.Success(
         data = listOf(
-            RepositoryUiState(
+            RepositoryUiModel(
                 id = 1,
                 fullName = "next-step/nextstep-docs",
                 description = "nextstep 매뉴얼 및 문서를 관리하는 저장소",
+                stars = 50,
+                isOverFiftyStars = true,
             ),
-            RepositoryUiState(
+            RepositoryUiModel(
                 id = 2,
                 fullName = "next-step/holy-moly",
                 description = "nextstep 홀리몰리한 저장소",
+                stars = 100,
+                isOverFiftyStars = true,
             ),
-            RepositoryUiState(
+            RepositoryUiModel(
                 id = 3,
                 fullName = "next-step/haly-galy",
                 description = "nextstep 할리갈리한 저장소",
+                stars = 49,
+                isOverFiftyStars = false,
             ),
         )
     )
@@ -46,7 +56,7 @@ class StatelessGithubScreenTest {
     fun Github_레포지토리_데이터가_정상적으로_화면에_출력된다() {
         composeTestRule.setContent {
             GithubScreen(
-                repositoryUiState = uiState,
+                repositoryUiModel = uiState,
                 snackbarHostState = SnackbarHostState()
             )
         }
@@ -61,7 +71,7 @@ class StatelessGithubScreenTest {
     fun Github_레포지토리_데이터가_빈값일시_화면에_메시지가_출력된다() {
         composeTestRule.setContent {
             GithubScreen(
-                repositoryUiState = UiState.Empty,
+                repositoryUiModel = UiState.Empty,
                 snackbarHostState = SnackbarHostState()
             )
         }
@@ -75,7 +85,7 @@ class StatelessGithubScreenTest {
     fun Github_레포지토리_데이터가_로딩일시_로딩바가_화면에_출력된다() {
         composeTestRule.setContent {
             GithubScreen(
-                repositoryUiState = UiState.Loading,
+                repositoryUiModel = UiState.Loading,
                 snackbarHostState = SnackbarHostState()
             )
         }
@@ -92,7 +102,7 @@ class StatelessGithubScreenTest {
 
         composeTestRule.setContent {
             GithubScreen(
-                repositoryUiState = UiState.Empty,
+                repositoryUiModel = UiState.Empty,
                 snackbarHostState = snackbarHostState
             )
         }
@@ -122,7 +132,7 @@ class StatelessGithubScreenTest {
 
         composeTestRule.setContent {
             GithubScreen(
-                repositoryUiState = UiState.Empty,
+                repositoryUiModel = UiState.Empty,
                 snackbarHostState = snackbarHostState
             )
         }
@@ -148,5 +158,48 @@ class StatelessGithubScreenTest {
 
         composeTestRule.onNodeWithContentDescription("Snackbar")
             .assertIsNotDisplayed()
+    }
+
+    @Test
+    fun 화면에_Star_갯수가_표시된다() {
+        composeTestRule.setContent {
+            GithubScreen(
+                repositoryUiModel = uiState,
+                snackbarHostState = SnackbarHostState()
+            )
+        }
+
+        uiState.data.forEach {
+            composeTestRule.onNodeWithText(it.stars.toString())
+                .assertExists()
+        }
+    }
+
+    @Test
+    fun Star_갯수가_50개_이상일때_Star_아이콘이_표시된다() {
+        composeTestRule.setContent {
+            GithubScreen(
+                repositoryUiModel = uiState,
+                snackbarHostState = SnackbarHostState()
+            )
+        }
+
+        composeTestRule.onNodeWithText("next-step/nextstep-docs")
+            .onParent()
+            .assert(hasAnyDescendant(hasText("HOT")))
+    }
+
+    @Test
+    fun Github_데이터중_star_갯수가_50_미만_이면_HOT이_표시되지_않는다() {
+        composeTestRule.setContent {
+            GithubScreen(
+                repositoryUiModel = uiState,
+                snackbarHostState = SnackbarHostState()
+            )
+        }
+
+        composeTestRule.onNodeWithText("next-step/haly-galy")
+            .onParent()
+            .assert(hasAnyDescendant(hasText("HOT").not()))
     }
 }

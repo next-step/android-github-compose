6366f4861f592d9b243403795ef7f9be94d94a12
fbghgus123 <fbghgus123@naver.com>
2025-03-18 20:09:37 +0900

    refactor: 리뷰 반영
    - Preview interactiveMode에서 스낵바가 노출되도록 수정

--- app/src/main/java/nextstep/github/ui/RepositoryListScreen.kt
+++ app/src/main/java/nextstep/github/ui/RepositoryListScreen.kt
@@ -1,9 +1,11 @@
 package nextstep.github.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
@@ -12,12 +14,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.launch
 import nextstep.github.data.entity.Repository
 import nextstep.github.ui.component.RepositoryListContent
 import nextstep.github.ui.component.RepositoryListEmptyContent
@@ -159,12 +163,25 @@ private fun RepositoryListEmptyScreenPreview() {
     }
 }
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Preview
 @Composable
 private fun RepositoryListErrorScreenPreview() {
     GithubTheme {
+        val scope = rememberCoroutineScope()
+        val snackBarHostState = SnackbarHostState()
+
+        scope.launch {
+            snackBarHostState.showSnackbar(
+                message = "예상치 못한 오류가 발생하였습니다.",
+                actionLabel = "재시도",
+                duration = SnackbarDuration.Indefinite,
+            )
+        }
+
         RepositoryListScreen(
-            uiState = RepositoryListScreenUiState.Error
+            uiState = RepositoryListScreenUiState.Error,
+            snackBarHostState = snackBarHostState
         )
     }
 }

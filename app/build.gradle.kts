30e20a0d280cd0cc444fcfe484bd15ae976c72ca
seungohjin <corrobolea@gmail.com>
2025-03-18 15:19:29 +0900

    feat: mockk, coroutines.test 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    androidTestImplementation(libs.mockk.android)
+    testImplementation(libs.kotlinx.coroutines.test)
 }

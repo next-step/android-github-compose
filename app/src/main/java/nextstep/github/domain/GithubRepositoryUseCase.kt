5007347e1b2dd9d6dcf7a6bf0cb6467fce7e4fdd
seungohjin <corrobolea@gmail.com>
2025-03-18 12:42:44 +0900

    refactor: GithubRepositoryUseCase getRepositories 예외처리 로직 개선

--- app/src/main/java/nextstep/github/domain/GithubRepositoryUseCase.kt
+++ app/src/main/java/nextstep/github/domain/GithubRepositoryUseCase.kt
@@ -1,27 +1,29 @@
 package nextstep.github.domain
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import nextstep.github.data.repository.GithubRepository
 import nextstep.github.domain.model.Repository
 
 class GithubRepositoryUseCase(private val githubRepository: GithubRepository) {
 
-    fun getRepositories(organization: String): Flow<RepositoryResult<List<Repository>>> = flow {
-        githubRepository.getRepositories(organization)
-            .onSuccess { repositories ->
-                val data = repositories.map {
-                    Repository(
-                        id = it.id,
-                        fullName = it.fullName,
-                        description = it.description,
-                        stars = it.stars,
-                    )
-                }
+    fun getRepositories(organization: String): Flow<RepositoryResult<List<Repository>>> =
+        flow<RepositoryResult<List<Repository>>> {
+            val data = githubRepository.getRepositories(organization)
+                .map {
+                    it.map { repositoryDto ->
+                        Repository(
+                            id = repositoryDto.id,
+                            fullName = repositoryDto.fullName,
+                            description = repositoryDto.description,
+                            stars = repositoryDto.stars,
+                        )
+                    }
+                }.getOrThrow()
 
-                emit(RepositoryResult.Success(data))
-            }.onFailure { exception ->
-                emit(RepositoryResult.Error(exception))
-            }
-    }
+            emit(RepositoryResult.Success(data))
+        }.catch {
+            emit(RepositoryResult.Error(it))
+        }
 }

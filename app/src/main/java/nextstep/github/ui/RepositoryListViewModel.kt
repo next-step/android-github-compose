2f94e07d533aa67684aca1c7b4858ed2c4ce1045
hyemdooly <hyemdooly@gmail.com>
2025-04-03 00:27:37 +0900

    refactor: flow 초기화 함수 분리

--- app/src/main/java/nextstep/github/ui/RepositoryListViewModel.kt
+++ app/src/main/java/nextstep/github/ui/RepositoryListViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.AP
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
@@ -17,22 +18,24 @@ import nextstep.github.ui.model.RepositoryListUiState
 class RepositoryListViewModel(
     private val githubRepoRepository: GithubRepoRepository
 ) : ViewModel() {
-    val uiState = flow {
-        emit(RepositoryListUiState.Loading(error = false))
-
-        val repositories = githubRepoRepository.getRepositories(ORGANIZATION)
-        if (repositories.isEmpty()) {
-            emit(RepositoryListUiState.Empty)
-        } else {
-            emit(RepositoryListUiState.Success(items = repositories))
-        }
-    }.catch {
-        emit(RepositoryListUiState.Loading(error = true))
-    }.stateIn(
+    val uiState = getRepositoriesFlow().stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
         initialValue = RepositoryListUiState.Loading(error = false)
     )
+
+    private fun getRepositoriesFlow(): Flow<RepositoryListUiState> {
+        return flow {
+            val repositories = githubRepoRepository.getRepositories(ORGANIZATION)
+            if (repositories.isEmpty()) {
+                emit(RepositoryListUiState.Empty)
+            } else {
+                emit(RepositoryListUiState.Success(items = repositories))
+            }
+        }.catch {
+            emit(RepositoryListUiState.Loading(error = true))
+        }
+    }
 
     companion object {
         private const val ORGANIZATION = "next-step"

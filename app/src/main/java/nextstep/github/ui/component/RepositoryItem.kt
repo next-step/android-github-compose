ba836d067aec6035aebd256a638684fe1349b6e5
hyemdooly <hyemdooly@gmail.com>
2025-04-04 00:37:02 +0900

    feat: 저장소의 Star 개수 노출, HOT 텍스트 노출 구현

--- app/src/main/java/nextstep/github/ui/component/RepositoryItem.kt
+++ app/src/main/java/nextstep/github/ui/component/RepositoryItem.kt
@@ -2,18 +2,30 @@ package nextstep.github.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import nextstep.github.R
 import nextstep.github.data.repository.model.RepositoryEntity
 import nextstep.github.ui.theme.GithubTheme
+import nextstep.github.util.orZero
 
 @Composable
 fun RepositoryItem(
@@ -25,8 +37,25 @@ fun RepositoryItem(
             .background(color = MaterialTheme.colorScheme.surface)
     ) {
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
         ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                if (repository.stars.orZero() >= 50) {
+                    Text(
+                        text = stringResource(R.string.repository_item_hot),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                StarCount(
+                    count = repository.stars.orZero(),
+                )
+            }
             Text(
                 text = repository.fullName.orEmpty(),
                 style = MaterialTheme.typography.titleLarge,
@@ -42,16 +71,47 @@ fun RepositoryItem(
     }
 }
 
+@Composable
+fun StarCount(
+    count: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            modifier = Modifier.size(18.dp),
+            imageVector = Icons.Filled.Star,
+            contentDescription = null
+        )
+        Text(
+            text = count.toString(),
+            style = MaterialTheme.typography.labelLarge,
+        )
+    }
+}
+
+private class StarCountPreviewParameterProvider : PreviewParameterProvider<Int> {
+    override val values = sequenceOf(
+        10,
+        50
+    )
+}
+
+
 @Preview
 @Composable
-private fun RepositoryItemPreview() {
+private fun RepositoryItemPreview(
+    @PreviewParameter(StarCountPreviewParameterProvider::class) star: Int
+) {
     GithubTheme {
         RepositoryItem(
             RepositoryEntity(
                 id = 0,
                 fullName = "next-step/nextstep-docs",
                 description = "nextstep 매뉴얼 및 문서를 관리하는 저장소",
-                stars = 500
+                stars = star
             ),
             modifier = Modifier.fillMaxWidth()
         )
---
+++ app/src/main/java/nextstep/github/util/IntUtil.kt
@@ -0,0 +1,3 @@
+package nextstep.github.util
+
+fun Int?.orZero(): Int = this ?: 0
